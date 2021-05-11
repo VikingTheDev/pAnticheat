4e059f37d06a5f895121c380b25a9db6ef4f51eb
VikingTheDev <64926058+VikingTheDev@users.noreply.github.com>
2021-05-11 13:41:02 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,11 @@ game 'common'
 
 server_script "src/server/index.js"
 
+dependencies {
+  'screenshot-basic',
+  'yarn'
+}
+
 author 'Petrikov'
 description 'Filters out vulgar language'
 version '1.0.0'
